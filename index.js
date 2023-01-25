19b4b57f775c89d27cb2539661ed321278bdb179
lucianacappa <102822459+lucianacappa@users.noreply.github.com>
2023-01-25 17:05:54 +0100

    Ejercicios de JavaScript
    Ejercicios relacioados a Js

---
+++ index.js
@@ -0,0 +1,7 @@
+const volumen = 16*23*13;
+console.log ('volumen', volumen + 'cm3');
+
+const arealateral = 23*23; 
+const areabase = 16*23;
+const areaTotal = arealateral + 2*areabase
+console.log('areaTotal' + areaTotal + 'cm2');
