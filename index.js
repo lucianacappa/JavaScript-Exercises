4fed3b6802b673ec1579ab485d57a0843eab185f
lucianacappa <102822459+lucianacappa@users.noreply.github.com>
2023-01-26 09:50:47 +0100

    update

--- index.js
+++ index.js
@@ -30,3 +30,32 @@ console.log ('Mi cumpleaños es el ' + cumple.dia + ' de '+ cumple.mes);
 
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril','Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 console.log('El mes siguiente a mi cumpleaños es:' +meses[4] )
+
+//Ejercicio 3
+
+const data = ['hola', 2,5,'adios'];
+console.log(data[1]+ data[2])
+console.log(data[1]> data[2])
+
+//Ejercicio 4
+
+var numero1 = 5;
+var numero2 = 8; 
+
+if (numero1 <= numero2) {
+    alert (numero1 +'no es mayor que' + numero2);
+}
+
+if (numero2 > 0) {
+    alert (numero2 +'es positivo');
+}
+
+if (numero1 < 0) {
+    alert (numero1 +'es negativo o distinto de cero');
+}
+
+if (numero1 +1 <= numero2) {
+    alert ('Incrementar en 1 unidad el valor de' + numero1 + 'no lo hace mayor o igual que' + numero2);
+}
+
+//Ejercicio 5
