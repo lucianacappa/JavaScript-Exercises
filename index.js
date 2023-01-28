a02b670fade33622d8e743f930032e875799a72f
lucianacappa <lucianacappa@gmail.com>
2023-01-28 11:26:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -193,3 +193,32 @@ const users = [
     }
     
     console.log(nonPremiumUsers);
+
+// Ejercicio 16 
+class geometria {
+    constructor (lado){
+        this.lado = lado;
+    }
+
+    perimetro(){
+        return this.lado*4
+    }
+
+    area(){
+        return this.lado * this.lado
+    }
+}
+
+const cuadradopequeño = new geometria(2);
+console.log("El perímetro del cuadrado pequeño es: " + cuadradopequeño.perimetro() + "cm");
+console.log("Area of small square: " + cuadradopequeño.area() + "cm2");
+
+const cuadradomedio = new geometria(5);
+console.log("El perímetro del cuadrado pequeño es: " + cuadradomedio.perimetro() + "cm");
+console.log("Area of small square: " + cuadradomedio.area() + "cm2");
+
+const cuadradogrande = new geometria(2);
+console.log("El perímetro del cuadrado pequeño es: " + cuadradogrande.perimetro() + "cm");
+console.log("Area of small square: " + cuadradogrande.area() + "cm2");
+
+// Ejercicio 17
